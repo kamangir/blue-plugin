f8b2a432a58e6d120fe94d94cd37c1b3070eba37
kamangir <arash@kamangir.net>
2022-09-18 12:05:24 -0700

    validating Kanata - kamangir/bolt#709

--- .abcli/blue_plugin.sh
+++ .abcli/blue_plugin.sh
@@ -8,7 +8,7 @@ function blue_plugin() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_help_line "abct task_1" \
+        abcli_show_usage "abct task_1" \
             "run abct task_1."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then

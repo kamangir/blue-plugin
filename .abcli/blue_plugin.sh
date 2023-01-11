798fbe805a080c0634dd4bf01975e4cec6dd158d
kamangir <arash@kamangir.net>
2023-01-10 21:10:48 -0800

    aiart clean-up - kamangir/bolt#741

--- .abcli/blue_plugin.sh
+++ .abcli/blue_plugin.sh
@@ -8,10 +8,11 @@ function blue_plugin() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
+        # blue_plugin_task $@
+
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m blue_plugin --help
         fi
-
         return
     fi
 
@@ -22,9 +23,7 @@ function blue_plugin() {
     fi
 
     if [ "$task" == "version" ] ; then
-        python3 -m blue_plugin \
-            version \
-            ${@:2}
+        abcli_log $(python3 -m blue_plugin version ${@:2})
         return
     fi
 
--- blue_plugin/__init__.py
+++ blue_plugin/__init__.py
@@ -1,5 +1,5 @@
 NAME = "blue_plugin"
 
-VERSION = "2.59.1"
+VERSION = "2.60.1"
 
 from .functions import *

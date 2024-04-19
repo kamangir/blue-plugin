989f5ac84a0c24b0cf297c254b52efab8d0c6180
kamangir <arash@kamangir.net>
2024-04-18 18:48:27 -0700

    env refactor - kamangir/bolt#123

--- blue_plugin/env.py
+++ blue_plugin/env.py
@@ -1,8 +1,8 @@
 import os
 from abcli.env import load_env, load_config
 
-load_env(__name__)
-load_config(__name__)
+load_env(__name__, True)
+load_config(__name__, True)
 
 
 BLUE_PLUGIN_SECRET = os.getenv("BLUE_PLUGIN_SECRET", "")

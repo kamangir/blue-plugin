f12dfd6a1688bda006d9718a3cd89106c25591eb
kamangir <arash@kamangir.net>
2024-05-19 15:46:26 -0700

    pypi rebuild - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
+import os
 
 from blue_plugin import NAME, VERSION, DESCRIPTION
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as f:
+    long_description = f.read().replace(
+        "./",
+        "https://github.com/kamangir/awesome-bash-cli/raw/current/",
+    )
+
 
 setup(
     name=NAME,
     author="arash@kamangir.net",
     version=VERSION,
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=[NAME],
     package_data={
         NAME: ["config.env"],

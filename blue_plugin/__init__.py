afec48df1491cea1b21f394e6bf87932b563e8ec
kamangir <arash@kamangir.net>
2024-05-19 15:51:07 -0700

    pypi rebuild - kamangir/bolt#746

--- blue_plugin/__init__.py
+++ blue_plugin/__init__.py
@@ -2,6 +2,6 @@ NAME = "blue_plugin"
 
 ICON = "🌀"
 
-DESCRIPTION = f"{ICON} a template for an awesome-bash-cli plugin."
+DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
 
-VERSION = "2.166.1"
+VERSION = "2.167.1"

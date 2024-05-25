4af1923dfebd424eb610a7a3fca7172bcf4e8ae1
kamangir <arash@kamangir.net>
2024-05-24 18:58:58 -0700

    refactor - kamangir/bolt#746

---
+++ .abcli/install/template.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+function abcli_install_blue_plugin_template() {
+    :
+}
+
+abcli_install_module blue_plugin_template 101
--- blue_plugin/__init__.py
+++ blue_plugin/__init__.py
@@ -4,4 +4,4 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
 
-VERSION = "3.45.1"
+VERSION = "3.46.1"

1c64b1680238634746682b76d2c8f8d31cb7efed
kamangir <arash@kamangir.net>
2024-05-25 21:30:22 -0700

    bash delivery - kamangir/bolt#746

--- .abcli/actions.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-function blue_plugin_action_git_before_push() {
-    blue_plugin pypi build
-}
--- blue_plugin/__init__.py
+++ blue_plugin/__init__.py
@@ -4,4 +4,4 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
 
-VERSION = "3.50.1"
+VERSION = "3.51.1"

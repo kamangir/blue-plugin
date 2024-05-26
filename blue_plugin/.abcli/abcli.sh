aac49021a003ab4e734214f304811ecffda3d080
kamangir <arash@kamangir.net>
2024-05-26 00:41:28 -0700

    refactor - kamangir/bolt#746

--- blue_plugin/.abcli/abcli.sh
+++ blue_plugin/.abcli/abcli.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-abcli_source_path \
-    $abcli_path_git/blue-plugin/.abcli/tests
+abcli_source_path - caller,suffix=/tests
 
 abcli_env dot load \
     plugin=blue_plugin
--- blue_plugin/__init__.py
+++ blue_plugin/__init__.py
@@ -4,4 +4,4 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
 
-VERSION = "3.51.1"
+VERSION = "3.52.1"

6bc87092719a84d23b7b539794a37ead03494d49
kamangir <arash@kamangir.net>
2024-07-09 19:38:36 -0700

    += marquee - kamangir/bolt#746

--- blue_plugin/__init__.py
+++ blue_plugin/__init__.py
@@ -4,6 +4,10 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} a git template for an awesome-bash-cli plugin."
 
-VERSION = "3.83.1"
+VERSION = "3.84.1"
 
 REPO_NAME = "blue-plugin"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/blob/main/blue-plugin/marquee.png?raw=true"
+)

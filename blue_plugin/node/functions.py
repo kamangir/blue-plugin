36da46be5b302ded570a9fba21e05da6312a3a4c
kamangir <arash@kamangir.net>
2024-07-28 13:31:44 -0700

    start of blueness-update-2024-07-28 🪄 - kamangir/bolt#746

--- blue_plugin/node/functions.py
+++ blue_plugin/node/functions.py
@@ -1,6 +1,11 @@
+from blueness import module
+from blue_plugin import NAME
 from blue_plugin.logger import logger
 
 
+NAME = module.name(__file__, NAME)
+
+
 def func(arg: str) -> bool:
-    logger.info(f"arg:{arg}")
+    logger.info(f"{NAME}.func: arg={arg}")
     return True

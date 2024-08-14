09414bb165e0818911e91c9ebe23d8f4ec9182ab
kamangir <arash@kamangir.net>
2024-08-13 19:01:09 -0700

    start of ssm-2024-08-13-64930 🪄 - kamangir/bolt#746

--- blue_plugin/.abcli/abcli.sh
+++ blue_plugin/.abcli/abcli.sh
@@ -2,7 +2,11 @@
 
 abcli_source_path - caller,suffix=/tests
 
-abcli_env dot load \
-    plugin=blue_plugin
+if [[ "$abcli_is_docker" == true ]]; then
+    abcli_ssm get plugin=blue_plugin
+else
+    abcli_env dot load plugin=blue_plugin
+fi
+
 abcli_env dot load \
     filename=blue_plugin/config.env,plugin=blue_plugin

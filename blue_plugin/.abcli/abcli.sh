94ebe3a3e5c618b2d94d2afae7118a4d4f36d362
kamangir <arash@kamangir.net>
2024-08-15 00:26:18 -0700

    @env refactor - kamangir/bolt#746

--- blue_plugin/.abcli/abcli.sh
+++ blue_plugin/.abcli/abcli.sh
@@ -4,7 +4,7 @@ abcli_source_path - \
     caller,suffix=/tests
 
 abcli_env_dot_load \
-    caller,ssm,suffix=/../..
+    caller,ssm,plugin=blue_plugin,suffix=/../..
 
 abcli_env_dot_load \
     caller,filename=config.env,suffix=/..

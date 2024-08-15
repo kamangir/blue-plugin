bd3b7942edfd0295814e8add57d7db12e1ce0f97
kamangir <arash@kamangir.net>
2024-08-14 21:10:14 -0700

    wip - kamangir/bolt#746

--- blue_plugin/.abcli/abcli.sh
+++ blue_plugin/.abcli/abcli.sh
@@ -1,12 +1,13 @@
 #! /usr/bin/env bash
 
-abcli_source_path - caller,suffix=/tests
+abcli_source_path - \
+    caller,suffix=/tests
 
-if [[ "$abcli_is_docker" == true ]]; then
-    abcli_ssm get plugin=blue_plugin
-else
-    abcli_env dot load plugin=blue_plugin
-fi
+#abcli_env dot load \
+#    caller,plugin=blue_plugin,ssm,suffix=/../..
+
+abcli_env dot load \
+    plugin=blue_plugin
 
 abcli_env dot load \
     filename=blue_plugin/config.env,plugin=blue_plugin

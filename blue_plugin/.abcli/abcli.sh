6aead5b947f1d4f3f7056f9786487606b9f07733
kamangir <arash@kamangir.net>
2024-08-14 23:50:10 -0700

    redundancy removed - kamangir/bolt#746

--- blue_plugin/.abcli/abcli.sh
+++ blue_plugin/.abcli/abcli.sh
@@ -4,7 +4,7 @@ abcli_source_path - \
     caller,suffix=/tests
 
 abcli_env_dot_load \
-    caller,plugin=blue_plugin,ssm,suffix=/../..
+    caller,ssm,suffix=/../..
 
 abcli_env dot load \
     filename=blue_plugin/config.env,plugin=blue_plugin

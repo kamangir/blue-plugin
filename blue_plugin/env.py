c42f1cbff1f6fb8d53d1d8eedeb4bf30c003ccb4
kamangir <arash@kamangir.net>
2025-02-07 23:18:55 -0800

    start of upstream-refactor-2025-02-07-4oyvv8 🪄 - kamangir/bolt#746

--- blue_plugin/env.py
+++ blue_plugin/env.py
@@ -1,16 +1,10 @@
 import os
-from blue_options.env import load_config, load_env
+from blue_options.env import load_config, load_env, get_env
 
 load_env(__name__)
 load_config(__name__)
 
 
-BLUE_PLUGIN_SECRET = os.getenv(
-    "BLUE_PLUGIN_SECRET",
-    "",
-)
+BLUE_PLUGIN_SECRET = get_env("BLUE_PLUGIN_SECRET")
 
-BLUE_PLUGIN_CONFIG = os.getenv(
-    "BLUE_PLUGIN_CONFIG",
-    "",
-)
+BLUE_PLUGIN_CONFIG = get_env("BLUE_PLUGIN_CONFIG")
